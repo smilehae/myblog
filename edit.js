5cf7b8b5d62cd098b07b3532048ed698c9a84338
smilehae <mihaepyun@gmail.com>
2021-12-08 22:25:11 +0900

    기능 추가 : localStorage 저장 ) 새로고침 시 내용 날라가지 않도록

--- edit.js
+++ edit.js
@@ -1,16 +1,28 @@
 const writeSpace = document.querySelector("input[type='textarea']");
 const inputContainer = Array.from(document.querySelectorAll("input, select"));
 const submitBtn = document.querySelector("input[type='submit']");
-
 let postData = {};
+postData = JSON.parse(localStorage.getItem("postData", postData)) || {};
 
 function getPostData() {
   const postData = {};
   inputContainer.forEach((input) => {
     if (input.id != "submit") postData[input.id] = input.value;
   });
+  localStorage.setItem("postData", JSON.stringify(postData));
   return postData;
 }
+
+// 새로고침했을때도 값이 있었음 좋겠다
+function setPostData() {
+  inputContainer.forEach((input) => {
+    if (input.id != "submit") {
+      input.value = postData[input.id];
+    }
+  });
+}
+
+setPostData();
 
 writeSpace.addEventListener("keydown", (e) => {
   if (e.keyCode == 13) {
