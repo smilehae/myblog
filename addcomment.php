b722b8cf14ca4be486bd39658fc4a747056c8e6f
smilehae <mihaepyun@gmail.com>
2021-12-19 14:47:47 +0900

    댓글 입력 후 리다잍렉트 시 해당 페이지로 다이렉트!

--- addcomment.php
+++ addcomment.php
@@ -8,10 +8,7 @@
             $nickname = $_POST['nickname'];
             $pw = $_POST['pw'];
             $content = $_POST['content'];
-            
-            echo "$pid $nickname $pw $content";
-
-            
+                       
             $conn = mysqli_connect("127.0.0.1","root");
             mysqli_set_charset($conn,"utf8");
             if(!$conn){
@@ -23,14 +20,14 @@
             //$result = mysqli_query($conn,"select * from comments");
             $result = mysqli_query($conn, "insert into comments(nickname, pw,content,pid) values ('$nickname','$pw','$content','$pid')");
             if($result){
-                echo "트랜잭션 성공";
+               // echo "트랜잭션 성공";
             }
             else{
                 // 트랜잭션에 대한 에러 메시지 확인
                 echo "트랜잭션 실패" .$conn->error;
             }
             mysqli_close($conn);
-            header('Location: ./index.php')
+            echo "<script>window.location.href='./subpage.php?id=$pid';</script>";
             ?>
     </body>
 </html>
